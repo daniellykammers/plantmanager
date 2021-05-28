2c4c826afa8ad1321bc4cc5e577e2bdd5d710f3d
jdreldev <desenvolvimento@jdrel.com.br>
2021-05-28 18:50:34 -0300

    lesson3

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -9,6 +9,7 @@ import {
     TouchableWithoutFeedback,
     Platform,
     Keyboard,
+    Alert,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 
@@ -38,6 +39,7 @@ export function UserIdentification() {
     }
 
     function handleSubmit() {
+        if (!name) return Alert.alert('Me diz como chamar você 😢');
         navigation.navigate('Confirmation');
     }
 
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+//componentes
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 
 import colors from '../styles/colors';
 
@@ -31,6 +33,11 @@ const AppRoutes: React.FC = () => (
         <stackRoutes.Screen
             name="Confirmation"
             component={Confirmation}
+        ></stackRoutes.Screen>
+
+        <stackRoutes.Screen
+            name="PlantSelect"
+            component={PlantSelect}
         ></stackRoutes.Screen>
     </stackRoutes.Navigator>
 );
